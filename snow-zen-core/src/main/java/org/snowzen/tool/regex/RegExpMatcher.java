a9a1eeec9398daa169223c54d9aae7d724cd8644
snow-zen <13517064839@139.com>
2020-10-08 21:08:49 +0800

    调整内部结构

--- snow-zen-core/src/main/java/org/snowzen/tool/regex/RegExpMatcher.java
+++ snow-zen-core/src/main/java/org/snowzen/tool/regex/RegExpMatcher.java
@@ -1,95 +1,105 @@
 package org.snowzen.tool.regex;
 
+import java.util.regex.Pattern;
+
 import static org.snowzen.tool.regex.RegExpConstant.*;
 
 /**
- * 正则匹配器，依赖于{@link RegExpConstant}中定义的规则进行匹配
+ * 正则匹配器
  *
  * @author sobre
+ * @see RegExpConstant
  */
 public class RegExpMatcher {
 
+    private static boolean match(Pattern pattern, CharSequence input) {
+        if (input != null && input.length() > 0) {
+            return pattern.matcher(input).matches();
+        }
+        throw new NullPointerException("input");
+    }
+
     public static boolean matchMobilePhone(CharSequence input) {
-        return MOBILE_PHONE_MATCH.matcher(input).matches();
+        return match(MOBILE_PHONE_MATCH, input);
     }
 
     public static boolean matchPhone(CharSequence input) {
-        return PHONE_MATCH.matcher(input).matches();
+        return match(PHONE_MATCH, input);
     }
 
     public static boolean matchEmail(CharSequence input) {
-        return EMAIL_MATCH.matcher(input).matches();
+        return match(EMAIL_MATCH, input);
     }
 
     public static boolean matchIdCard(CharSequence input) {
-        return ID_CARD_MATCH.matcher(input).matches();
+        return match(ID_CARD_MATCH, input);
     }
 
     public static boolean matchNumber(CharSequence input) {
-        return NUMBER_MATCH.matcher(input).matches();
+        return match(NUMBER_MATCH, input);
     }
 
     public static boolean matchChinese(CharSequence input) {
-        return CHINESE_MATCH.matcher(input).matches();
+        return match(CHINESE_MATCH, input);
     }
 
     public static boolean matchLowercaseEnglish(CharSequence input) {
-        return LOWER_CASE_ENGLISH_MATCH.matcher(input).matches();
+        return match(LOWER_CASE_ENGLISH_MATCH, input);
     }
 
     public static boolean matchUppercaseEnglish(CharSequence input) {
-        return UPPER_CASE_ENGLISH_MATCH.matcher(input).matches();
+        return match(UPPER_CASE_ENGLISH_MATCH, input);
     }
 
     public static boolean matchEnglish(CharSequence input) {
-        return ENGLISH_MATCH.matcher(input).matches();
+        return match(ENGLISH_MATCH, input);
     }
 
     public static boolean matchDomain(CharSequence input) {
-        return DOMAIN_MATCH.matcher(input).matches();
+        return match(DOMAIN_MATCH, input);
     }
 
     public static boolean matchMonth(CharSequence input) {
-        return MONTH_MATCH.matcher(input).matches();
+        return match(MONTH_MATCH, input);
     }
 
     public static boolean matchDayOfMonth(CharSequence input) {
-        return DAY_OF_MONTH_MATCH.matcher(input).matches();
+        return match(DAY_OF_MONTH_MATCH, input);
     }
 
     public static boolean matchHtml(CharSequence input) {
-        return HTML_MATCH.matcher(input).matches();
+        return match(HTML_MATCH, input);
     }
 
     public static boolean matchHttp(CharSequence input) {
-        return HTTP_MATCH.matcher(input).matches();
+        return match(HTTP_MATCH, input);
     }
 
     public static boolean matchUrl(CharSequence input) {
-        return URL_MATCH.matcher(input).matches();
+        return match(URL_MATCH, input);
     }
 
     public static boolean matchIpv4(CharSequence input) {
-        return IP_V4_MATCH.matcher(input).matches();
+        return match(IP_V4_MATCH, input);
     }
 
     public static boolean matchIpv6(CharSequence input) {
-        return IP_V6_MATCH.matcher(input).matches();
+        return match(IP_V6_MATCH, input);
     }
 
     public static boolean matchHexColor(CharSequence input) {
-        return HEX_COLOR_MATCH.matcher(input).matches();
+        return match(HEX_COLOR_MATCH, input);
     }
 
     public static boolean matchLicensePlate(CharSequence input) {
-        return LICENSE_PLATE_MATCH.matcher(input).matches();
+        return match(LICENSE_PLATE_MATCH, input);
     }
 
     public static boolean matchCharacter(CharSequence input) {
-        return CHARACTER_MATCH.matcher(input).matches();
+        return match(CHARACTER_MATCH, input);
     }
 
     public static boolean matchMoney(CharSequence input) {
-        return MONEY_MATCH.matcher(input).matches();
+        return match(MONEY_MATCH, input);
     }
 }
